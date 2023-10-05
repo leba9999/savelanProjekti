97cbf46b46b97e7561545f8df5eacb7aaacb0154
HannaKaimo <hanna.kaimo@metropolia.fi>
2023-10-05 16:38:06 +0300

    tietokantayhteys javascriptilla

---
+++ backend/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'savelanprojekti'
+});
+// connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+  }
+});
+// close the MySQL connection
+connection.end()
