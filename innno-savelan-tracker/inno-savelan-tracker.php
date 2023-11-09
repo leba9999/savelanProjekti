e28a2fdf8779a783a5c3ae9af512d2c75790c7cf
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2023-11-09 13:41:18 +0200

    Async sending data
    Loading issue fixed by making data sending asynchronous and updated url to get permalink which hopefully fixes tracking parameters

--- innno-savelan-tracker/inno-savelan-tracker.php
+++ innno-savelan-tracker/inno-savelan-tracker.php
@@ -17,54 +17,45 @@ if( !defined('ABSPATH')){
 include_once('includes/admin-settings-page.php');
 
 function track_visitor() {
+
     $visitor_data = array(
         'ip' => $_SERVER['REMOTE_ADDR'],
         'user_agent' => $_SERVER['HTTP_USER_AGENT'],
         'timestamp' => date('Y-m-d H:i:s', current_time('timestamp')),
-        'url' => $_SERVER['REQUEST_URI'],
         'referrer' => $_SERVER['HTTP_REFERER'],
-        // Lisää tarvittaessa lisää tietoja
+        'url' => get_permalink(), // Get the current page's URL
+        // Include additional data as needed
     );
-    $backend_url = get_option('tracker_plugin_ip_domain', "http://localhost:3000/api/v1"); // Get the backend url from the admin settings page
-    /*
-    // Leevi - Data format testing. There was idea of creating different data formats for the data sent to the backend.
-    // XML format worked as well as json format. So we decided to use json format because it was familiar for all of us.
-    // This code is commented out because it is not used in the final version of the plugin.
-    // This code is left here for future use if needed.
 
-    $data_format = get_option('tracker_plugin_data_format', "json"); // Get the selected data format (JSON or XML)
-
-    BugFu::log($backend_url);
-    // Check the selected data format and construct the request accordingly
-    if ($data_format === 'xml') {
-        // Construct XML data
-        $xml = new SimpleXMLElement('<visitor_data/>');
-        array_walk_recursive($visitor_data, array ($xml, 'addChild'));
-        $request_body = $xml->asXML();
-        $content_type = 'application/xml';
-    } else {
-        $request_body = json_encode($visitor_data);
-        $content_type = 'application/json';
-    }
-    $test = wp_remote_get($backend_url);
-    BugFu::log($test);
-    */
+    $backend_url = get_option('tracker_plugin_ip_domain', 'http://localhost:3000/api/v1'); // Get the backend URL from the admin settings page
     $request_body = json_encode($visitor_data);
     $content_type = 'application/json';
+	
+	set_transient('visitor_data', $visitor_data, 60); // Adjust the timeout as needed
+    wp_schedule_single_event(time(), 'send_tracking_data_event');
+}
+// Function to actually send tracking data to the backend
+function send_tracking_data() {
+    $visitor_data = get_transient('visitor_data'); // Retrieve the data stored in transient
 
-    $response = wp_remote_post($backend_url, array(
-        'body' => $request_body,
-        'headers' => array('Content-Type' => $content_type),
-    ));
+    if ($visitor_data) {
+        $backend_url = get_option('tracker_plugin_ip_domain', 'http://localhost:3000/api/v1');
+        $request_body = json_encode($visitor_data);
+        $content_type = 'application/json';
 
-    if (is_wp_error($response)) {
-        // Error handling if the request fails
-        error_log('Failed to send data: ' . $response->get_error_message());
-    } else {
-        // Data sent successfully
+        // Send data to the backend using wp_remote_post
+        wp_remote_post($backend_url, array(
+            'body' => $request_body,
+            'headers' => array('Content-Type' => $content_type),
+        ));
+
+        // Data has been sent, remove the transient
+        delete_transient('visitor_data');
     }
 }
 
+// Hook the send_tracking_data function to the scheduled event
+add_action('send_tracking_data_event', 'send_tracking_data');
 add_action('wp_footer', 'track_visitor'); // Adds the tracking function to the footer of the website
 add_action('admin_menu', 'setup_menu'); // Adds the admin settings page to the admin menu
 ?>
