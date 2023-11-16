28b1e4e3e87c5b76d32a558d0280b7ac89e9494e
HeiniRinne <heini.e.rinne@gmail.com>
2023-11-16 11:48:36 +0200

    Error fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,9 +9,9 @@ DBConnection.initialize()
     // here you can start to work with your database
     logger.info("Data source has been initialized");
   })
-  .catch((error) => {
-    logger.error(error.message);
-    logger.debug(error.stack);
+  .catch((error : any) => {
+    logger.error(error?.message);
+    logger.debug(error?.stack);
   });
 
 try {
